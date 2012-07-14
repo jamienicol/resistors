79be8f87811ad34426927085ca88ab3495e2de1e
Jamie Nicol <jamie@thenicols.net>
2012-07-14 23:53:36 +0100

    Remove unnecessary import of LinearLayout.
    Accidentally imported. Not used, never has been.

--- src/org/jamienicol/resistors/ResistorsActivity.java
+++ src/org/jamienicol/resistors/ResistorsActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ResistorsActivity extends Activity
