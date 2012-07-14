71ef3247cb6e136713df44ce0aade23d0a8cc89a
Jamie Nicol <jamie@thenicols.net>
2012-07-14 17:48:17 +0100

    Fix indentation.

--- src/org/jamienicol/resistors/ResistorsActivity.java
+++ src/org/jamienicol/resistors/ResistorsActivity.java
@@ -2,14 +2,15 @@ package org.jamienicol.resistors;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class ResistorsActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
